9059d5744b5352b43346d3e8f3fbd6894746674c
vibeside <vibeside01@gmail.com>
2024-03-25 20:07:29 -0400

    doing stuff

---
+++ GiantFuckingRobot/RobotPlugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using System;
+
+namespace GiantFuckingRobot
+{
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class RobotPlugin : BaseUnityPlugin
+    {
+        public const string modGUID = "grug.lethalcompany.giantrobot";
+        public const string modName = "Giant Robot";
+        public const string modVersion = "09.04.2996";
+
+        public void Awake()
+        {
+
+        }
+    }
+}
