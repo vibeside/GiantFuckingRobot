edccb4b5de055508e60b5d8b5898eba012de57cc
vibeside <vibeside01@gmail.com>
2024-04-01 21:36:51 -0400

    made first patch, slight detour rq, bcz there's a base game bug

--- GiantFuckingRobot/RobotPlugin.cs
+++ GiantFuckingRobot/RobotPlugin.cs
@@ -1,6 +1,10 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 using MonoMod.RuntimeDetour;
 using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace GiantFuckingRobot
 {
@@ -10,10 +14,17 @@ namespace GiantFuckingRobot
         public const string modGUID = "grug.lethalcompany.giantrobot";
         public const string modName = "Giant Robot";
         public const string modVersion = "09.04.2996";
-        public static Hook? grabbableObjectHook 
+        public static ManualLogSource mls = null!;
+        public static Hook? grabbableObjectHook;
         public void Awake()
         {
-
+            mls = base.Logger;
+            grabbableObjectHook = new(typeof(StormyWeather).GetMethod(nameof(StormyWeather.LightningStrike),AccessTools.allDeclared),StormyWeatherPatch);
+        }
+        public static void StormyWeatherPatch(Action<StormyWeather, Vector3, bool> orig, StormyWeather self,Vector3 pos, bool atobj)
+        {
+            orig(self,pos,atobj);
+            mls.LogInfo(self.targetingMetalObject);
         }
     }
 }
